cb113e61f8730df5d0e1ea20b06d1d09d23e218c
Nishchithaln <nishchitha.l@simplify3x.com>
2023-02-10 17:08:44 +0530

    Added new modl functionalty

--- intern/src/App.js
+++ intern/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import StudentCourse from './component/StudentCourse';
 import "bootstrap/dist/css/bootstrap.min.css"
+import StudentTable from './component/StudentTable';
 
 
 function App() {
   return (
     <div className="App">
-     <StudentCourse/>
+     <StudentTable/>
+     {/* <StudentCourse/> */}
     </div>
   );
 }
